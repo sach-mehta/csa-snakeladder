0b6b68ba76e8c6e1b01868ca3bfc3077aa27fef8
Sachin Mehta <sachin.mehta@nagarro.com>
2023-01-10 22:16:48 +0530

    Initial commit for snake

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './styles/style.scss'
+import Snake from './components/Game/snake';
+const App = () => (
+  <div className="container">
+    <Snake></Snake>
+  </div>
+);
+
+export default App;
